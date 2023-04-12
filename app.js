4e725e559a51f6654517490ad2aa66e6efc06879
Dhairya <dhairyamagnetar@gmail.com>
2023-04-12 17:12:30 +0530

    minor change

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected with mongo atlas.");
-
 })
 
 const app = express();
